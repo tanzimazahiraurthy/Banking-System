75108a7e551974cca306c352f7592913a0f45a59
tanzimazahiraurthy <tanzimazahir02@gmail.com>
2020-05-09 20:33:24 +0600

    add all banking files

---
+++ BankingApplication.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.lang.*;
+
+
+public class BangingApplication
+{
+	public static void main(String[] args)
+	
+	{
+	 	 BankAccount obj1 = new BankAccount("xyz","BA0001");
+	  obj1.showMenu();
+	}
+	
+}
+
+
